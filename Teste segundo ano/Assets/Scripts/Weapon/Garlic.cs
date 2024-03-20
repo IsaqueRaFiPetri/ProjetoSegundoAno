adc75a01a2e9cf22f90c48ecdb1a44f0c78b33fe
IsaqueRaFiPetri <irfpetri@gmail.com>
2024-03-20 14:39:01 -0300

    Garlic & Cinemachine

--- Teste segundo ano/Assets/Scripts/Weapon/Garlic.cs
+++ Teste segundo ano/Assets/Scripts/Weapon/Garlic.cs
@@ -22,7 +22,15 @@ public class Garlic : MonoBehaviour
         timer = Time.deltaTime;
         if(timer >= waveRate)
         {
-
+            foreach(Enemy enemy in enemies) //mais otimisado
+            {
+                enemy.TakeDamage(damage);
+            }
+            timer = 0;
+            /*for(int i = 0; i < enemies.Count; i++)
+            {
+                enemies[i].TakeDamage(damage);
+            }*/
         }
     }
     private void OnTriggerEnter2D(Collider2D collision)
@@ -30,8 +38,25 @@ public class Garlic : MonoBehaviour
         enemies.Add(collision.GetComponent<Enemy>());
         if (!enemies[enemies.Count - 1].garliczed)
         {
-            enemies[enemies.Count - 1].TakeDamage(damage);
             enemies[enemies.Count - 1].garliczed = true;
+            enemies[enemies.Count - 1].TakeDamage(damage);
+        }
+        //collision.AddComponent<GarlicEffect>();
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        enemies.Remove(collision.GetComponent<Enemy>());
+        //enemies[2] = null; -> para array
+        //Destroy(collision.GetComponent<GarlicEffect>();
+    }
+    void NullEllementRemove()
+    {
+        for (int i = 0; i < enemies.Count; i++)
+        {
+            if (enemies[i] == null)
+            {
+                enemies.RemoveAt(i);
+            }
         }
     }
 }
