a64f451289be23903142b50f5fdc5c1fb34c0280
IsaqueRaFiPetri <irfpetri@gmail.com>
2024-03-26 14:01:30 -0300

    joystick

--- Mobile/Assets/Scripts/Player.cs
+++ Mobile/Assets/Scripts/Player.cs
@@ -6,7 +6,9 @@ using UnityEngine;
 [RequireComponent (typeof(BoxCollider2D))]
 public class Player : MonoBehaviour
 {
+    public Joystick joystick;
     Rigidbody2D body;
+    float horizontal, vertical;
 
     // Start is called before the first frame update
     void Start()
@@ -17,15 +19,19 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontal = joystick.Horizontal;
+        vertical = joystick.Vertical;
+        body.velocity = new Vector2(horizontal, vertical) * 5;
+        //para mudar a gravidade
         /*if (Input.GetMouseButtonDown(0))
         {
 
-        }*/
+        }
         if(Input.GetButtonDown("Fire1")) 
         {
             body.gravityScale *= -1;
             // se tivesse sito usado apenas o == -1, só poderia ser usado uma vez, se multiplicado, pode ser usasdo diversas vezes
-        }
-        
+        }*/
+
     }
 }
