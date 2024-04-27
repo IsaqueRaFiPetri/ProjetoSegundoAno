5c5571a4ec2cfc9dbc6db3af72ad826e48b5f616
IsaqueRaFiPetri <irfpetri@gmail.com>
2024-04-27 10:29:08 -0300

    Cross Begining

--- Teste segundo ano/Assets/Scripts/Weapon/Cross.cs
+++ Teste segundo ano/Assets/Scripts/Weapon/Cross.cs
@@ -6,16 +6,29 @@ public class Cross : WeaponBase
 {
     public int damage;
     public int speed;
+    public GameObject crossItem;
+    public Rigidbody2D body;
+    private float weaponCooldown;
    
     // Start is called before the first frame update
     void Start()
     {
-        
+        weaponCooldown = Time.deltaTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        FindClosest.FindClosestEnemy();
+        if(weaponCooldown == 2.0f)
+        {
+            FindClosest.FindClosestEnemy();
+            Shoot();
+        }
+        
+    }
+
+    void Shoot()
+    {
+        Instantiate(crossItem);
     }
 }
